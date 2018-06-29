bbcc3375aa678bd025ebfaa12b26db43d7639e4e
photon_rain <matt@lpcimac6.anu.edu.au>
2018-06-29 16:21:35 +1000

    Birthdays are now fuzzed.

--- scripts/interpolate_ages.py
+++ scripts/interpolate_ages.py
@@ -147,7 +147,7 @@ def estimate_birthdate(lifter_data):
     bdageidx = 0
     meetdateidx = 1
     # If a lifter has their birthday over the length of the meet we don't know
-    max_meetlength = 12
+    max_meetlength =12
 
     min_date = ''
     max_date = ''
@@ -192,11 +192,10 @@ def estimate_birthdate(lifter_data):
         else:  # We've managed to bound the birthdate
             by = init_year - (lower_age + 1)
             min_date = str(by)+'-'+min_date
-            max_datetime = datetime.date(
-                by, int(max_date.split('-')[0]), int(max_date.split('-')[1]))
+            max_datetime = datetime.date(by,int(max_date.split('-')[0]),int(max_date.split('-')[1]))
 
             fuzzed_end = max_datetime + datetime.timedelta(days=max_meetlength)
-
+            
             return [min_date, fuzzed_end.strftime("%Y-%m-%d")]
     else:  # No recorded ages, can't estimate a birthdate
         return []
