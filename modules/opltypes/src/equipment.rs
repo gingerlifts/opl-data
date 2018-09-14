6cb6750c21b37be7bbf1eb94cea045a3cf187999
photon_rain <matt@lpcimac6.anu.edu.au>
2018-09-14 10:53:58 +1000

    Fix Rust code. Mark 1983 APF Juniors as Raw Bench.

--- modules/opltypes/src/equipment.rs
+++ modules/opltypes/src/equipment.rs
@@ -1,45 +1,10 @@
 //! Defines the Equipment field.
 
 /// The Equipment field.
-#[derive(Clone, Copy, Debug, Deserialize, EnumString, PartialEq, Serialize)]
+#[derive(Clone, Copy, Debug, Deserialize, EnumString, PartialEq, Serialize, PartialOrd)]
 pub enum Equipment {
     Raw,
     Wraps,
-    #[serde(rename(deserialize = "Single-ply"))]
-    #[strum(serialize = "Single-ply")]
-    Single,
-    #[serde(rename(deserialize = "Multi-ply"))]
-    #[strum(serialize = "Multi-ply")]
-    Multi,
-    Straps,
-}
-
-#[derive(Clone, Copy, Debug, Deserialize, EnumString, PartialEq, Serialize)]
-pub enum SquatEquipment {
-    Raw,
-    Wraps,
-    #[serde(rename(deserialize = "Single-ply"))]
-    #[strum(serialize = "Single-ply")]
-    Single,
-    #[serde(rename(deserialize = "Multi-ply"))]
-    #[strum(serialize = "Multi-ply")]
-    Multi,
-}
-
-#[derive(Clone, Copy, Debug, Deserialize, EnumString, PartialEq, Serialize)]
-pub enum BenchEquipment {
-    Raw,
-    #[serde(rename(deserialize = "Single-ply"))]
-    #[strum(serialize = "Single-ply")]
-    Single,
-    #[serde(rename(deserialize = "Multi-ply"))]
-    #[strum(serialize = "Multi-ply")]
-    Multi,
-}
-
-#[derive(Clone, Copy, Debug, Deserialize, EnumString, PartialEq, Serialize)]
-pub enum DeadliftEquipment {
-    Raw,
     #[serde(rename(deserialize = "Single-ply"))]
     #[strum(serialize = "Single-ply")]
     Single,
