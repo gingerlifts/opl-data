cec7e23fb6069ff8932c4d9d116e8dee6f6ab16d
Michael Vessia <michael@vessia.net>
2019-04-09 15:57:43 +0000

    Update status.rs

--- server/src/pages/status.rs
+++ server/src/pages/status.rs
@@ -65,12 +65,13 @@ fn set_hardcoded_strings(statuses: &mut Vec<FederationStatus>) {
     statuses[BB as usize].status = complete;
     statuses[HERC as usize].status = complete;
     statuses[PA as usize].status = complete;
+    statuses[RPS as usize].status = complete;
     statuses[SCT as usize].status = complete;
     statuses[THSPA as usize].status = complete;
     statuses[THSWPA as usize].status = complete;
     statuses[USAPL as usize].status = "Since 2014";
     statuses[USPA as usize].status = complete;
-    statuses[RPS as usize].status = complete;
+    
 
     // Probes.
     let yes = "Yes";
