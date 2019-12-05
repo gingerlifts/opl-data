8862d36d213b508f888a7ef8db6cb219179e4419
Vitaliy Shvarz <vitaliy.shvarz.dev@gmail.com>
2019-12-05 16:22:19 +0100

    openipf fixes for mobile version

---
+++ server/client/scripts/global.ts
@@ -0,0 +1,36 @@
+function changeLanguage(): void {
+   const time = new Date();
+   time.setFullYear(time.getFullYear()+3);
+   const langselect = document.getElementById("langselect") as HTMLSelectElement;
+   const langselectValue = langselect.value || 'en';
+   const expireTime = time.toUTCString();
+
+   document.cookie="lang=" + langselectValue + "; expires=" + expireTime + "; path=/; ";
+   var h = window.location.href;
+   window.location.href = h.substring(0, h.indexOf("?"));
+}
+
+function changeUnits(): void {
+   const time = new Date();
+   time.setFullYear(time.getFullYear()+3);
+   const weightunits = document.getElementById("weightunits") as HTMLSelectElement;
+   const weightUnitsValue = weightunits.value || 'lbs';
+   const expireTime =  time.toUTCString();
+
+   document.cookie="units=" + weightUnitsValue + "; expires=" + expireTime + "; path=/; ";
+   window.location.href = window.location.href;
+}
+
+function initHeaderEventListeners(): void {
+  const weightunits = document.getElementById("weightunits") as HTMLSelectElement;
+  const langselect = document.getElementById("langselect") as HTMLSelectElement;
+
+  weightunits.addEventListener("change", changeUnits);
+  langselect.addEventListener("change", changeLanguage);
+}
+
+export {
+  changeLanguage,
+  changeUnits,
+  initHeaderEventListeners
+}
--- server/client/scripts/main.ts
+++ server/client/scripts/main.ts
@@ -4,42 +4,7 @@ import { loadMeetList } from './meetlist';
 import { loadRankingsScripts } from './rankings'
 import { loadRecordsScripts } from './records';
 import { isMobile } from './utils';
-
-function changeLanguage(): void {
-   const time = new Date();
-   time.setFullYear(time.getFullYear()+3);
-   const langselect = document.getElementById("langselect") as HTMLSelectElement;
-   const langselectValue = langselect.value || 'en';
-   const expireTime = time.toUTCString();
-
-   document.cookie="lang=" + langselectValue + "; expires=" + expireTime + "; path=/; ";
-   var h = window.location.href;
-   window.location.href = h.substring(0, h.indexOf("?"));
-}
-
-function changeUnits(): void {
-   const time = new Date();
-   time.setFullYear(time.getFullYear()+3);
-   const weightunits = document.getElementById("weightunits") as HTMLSelectElement;
-   const weightUnitsValue = weightunits.value || 'lbs';
-   const expireTime =  time.toUTCString();
-
-   document.cookie="units=" + weightUnitsValue + "; expires=" + expireTime + "; path=/; ";
-   window.location.href = window.location.href;
-}
-
-function initHeaderEventListeners(): void {
-  const weightunits = document.getElementById("weightunits") as HTMLSelectElement;
-  const langselect = document.getElementById("langselect") as HTMLSelectElement;
-
-  weightunits.addEventListener("change", changeUnits);
-  langselect.addEventListener("change", changeLanguage);
-}
-
-export {
-  changeLanguage,
-  changeUnits
-}
+import { initHeaderEventListeners } from './global';
 
 document.addEventListener("DOMContentLoaded", () => {
   const thisPage = window.location.pathname;
@@ -54,6 +19,22 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 
   initHeaderEventListeners();
+
+  if (thisPage.indexOf("/openipf")) {
+    if (thisPage.indexOf("openipf/m/") >= 0) {
+      loadMeetScripts();
+    } else if (thisPage.indexOf("openipf/mlist") >= 0) {
+      loadMeetList();
+    } else if (thisPage.indexOf("openipf/records") >= 0) {
+      loadRecordsScripts();
+    } else if(thisPage === '/dist/openipf' ||
+              thisPage === '/dist/openipf/' ||
+              thisPage.indexOf("openipf/rankings") >= 0){
+      loadRankingsScripts();
+    }
+
+    return;
+  }
 
   if (thisPage.indexOf("/m/") >= 0) {
     loadMeetScripts();
