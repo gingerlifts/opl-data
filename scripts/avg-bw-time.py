ced8fea8683c3a3f2ed6037892ade0b2f4882447
James Wakefield <jmw1982@gmail.com>
2020-06-24 09:50:02 +1000

    Add dict reader capabilities to CsvReadIter and make it generic for any line/row iterator, not just files.  Now outputs average bw%/days for each day delta.  We can use this to fit a curve

--- scripts/avg-bw-time.py
+++ scripts/avg-bw-time.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 
 import os
-import copy
 
 from datetime import datetime
+from statistics import mean
 
 from oplcsv import CsvReadIter
 
@@ -12,121 +12,129 @@ def opl_path(filename):
 
 def map_disamb_lifters(lifters):
 
-    name_i = lifters.index('Name')
-    lifter_id_i = lifters.index('LifterID')
-
     lifter_name_d = {} 
     lifter_id_set = set()
 
     for lifter_row in lifters:
-        lifter_name = lifter_row[name_i]
-        lifter_id = lifter_row[lifter_id_i]
 
         # we only care about disambiguated lifters as they have been
         # manually vetted in
 
-        if lifter_name.find('#') != -1:
-            lifter_name_d[lifter_id] = lifter_name
-            lifter_id_set.add(lifter_id)
+        if lifter_row['Name'].find('#') != -1:
+            lifter_name_d[lifter_row['LifterID']] = lifter_row['Name']
+            lifter_id_set.add(lifter_row['LifterID'])
 
     return (lifter_name_d, lifter_id_set,)
 
-def load_meet_data(meets):
 
-    meet_d = {}
+# augment entries with a datetime object from the date string in meets
+# only if it's in the set of lifter IDs we care about
+def augment_and_sort_entries(entries, meets, lifter_id_set):
 
-    meet_id_i = meets.index('MeetID')
-    date_i = meets.index('Date')
-    
-    for meet_row in meets:
-        meet_id = meet_row[meet_id_i]
-        meet_d[meet_id] = datetime.strptime(meet_row[date_i], "%Y-%m-%d")
+    meet_d = {meet_row['MeetID']: datetime.strptime(meet_row['Date'], "%Y-%m-%d") for meet_row in meets}
 
-    return meet_d
+    entry_list = []
+
+    for entry_row in entries:
+        if entry_row['LifterID'] in lifter_id_set and entry_row['BodyweightKg'] != '':
+            entry_row['DateObj'] = meet_d[entry_row['MeetID']]
+            entry_list.append(entry_row) 
+
+    sorted_entry_list = sorted(entry_list, key=lambda entry: entry['DateObj'])
+
+    return sorted_entry_list
 
 
-def collect_bw_data(entries, meet_d, lifter_id_set):
+def collect_bw_data(sorted_entry_list):
 
     bw_delta_list_d = {}
     bw_date_last_entry_d = {}
 
-    meet_id_i = entries.index('MeetID')
-    lifter_id_i = entries.index('LifterID')
-    bodyweight_i = entries.index('BodyweightKg')
+    for entry_row in sorted_entry_list:
+     
+        lifter_id = entry_row['LifterID']
+        meet_id = entry_row['MeetID']
+        meet_date = entry_row['DateObj']
+        bw_str = entry_row['BodyweightKg']
 
-    for entry_row in entries:
-        
-        lifter_id = entry_row[lifter_id_i] 
-        meet_id = entry_row[meet_id_i]
-        meet_date = meet_d[meet_id]
-        bw_str = entry_row[bodyweight_i]
+        bw_kg = float(bw_str)
 
-        # if the entry is for one of our disambiguated lifter, and we have a valid bw, we care
-        if lifter_id in lifter_id_set and bw_str != '':
+        # if this is the first entry for this lifter,
+        # we don't have a delta yet
+        last_entry_tup = bw_date_last_entry_d.get(lifter_id)
 
-            bw_kg = float(bw_str)
-
-            # if this is the first entry for this lifter,
-            # we don't have a delta yet
-            last_entry_tup = bw_date_last_entry_d.get(lifter_id)
-
-            if last_entry_tup:
-                (last_meet_id, last_meet_date, last_bw_kg,) = last_entry_tup
+        if last_entry_tup:
+            (last_meet_id, last_meet_date, last_bw_kg,) = last_entry_tup
             
-                # if multiple entries for the same meet, ignore
-                if last_meet_id != meet_id:
+            # if multiple entries for the same meet, ignore
+            # sometimes we also get the same lifter in different meets on the same day
+            # eg
+            # 10307,mags/plusa/1982-01-30-D,USPF,1981-11-07,USA,GA,Augusta,Southern Bench Press,
+            # 10817,mags/plusa/1981-12-21-A,USPF,1981-11-07,USA,GA,Augusta,Augusta Class II,
 
-                    if not bw_delta_list_d.get(lifter_id):
-                        bw_delta_list_d[lifter_id] = []
+            if last_meet_id != meet_id and last_meet_date != meet_date:
 
-                    delta_bw_kg = abs(bw_kg - last_bw_kg)
-                    delta_bw_pct = (delta_bw_kg / last_bw_kg) * 100.0
+                if not bw_delta_list_d.get(lifter_id):
+                    bw_delta_list_d[lifter_id] = []
 
-                    meet_date_td = meet_date - last_meet_date
-                    delta_days = abs(meet_date_td.days)
+                delta_bw_kg = abs(bw_kg - last_bw_kg)
+                delta_bw_pct = (delta_bw_kg / last_bw_kg) * 100.0
 
-                    bw_delta_list_d[lifter_id].append((delta_bw_pct, delta_days,))
+                meet_date_td = meet_date - last_meet_date
+                delta_days = meet_date_td.days
 
+                # if this is negative then we didn't sort properly
+                if delta_days < 0:
+                    raise ValueError("delta of days from one meet to next shouldn't be negative if meets were sorted!")
 
-            bw_date_last_entry_d[lifter_id] = (meet_id, copy.deepcopy(meet_date), bw_kg,)
+                bw_delta_list_d[lifter_id].append((delta_bw_pct, delta_days,))
+                
+        # datetime objects are deep copied implicitly
+        bw_date_last_entry_d[lifter_id] = (meet_id, meet_date, bw_kg,)
+
 
     return bw_delta_list_d
 
 
 def analyse_data(bw_delta_list_d):
 
-    avg_list = []
+    bw_pct_per_day2_d = {}
+    avg_bw_pct_per_day2_d = {}
 
-    # for each lifter, find the average bw pct per day
+    # for each lifter, find the bw pct per day and store it against
+    # the number of days so we can find the curve of d2(bw_pct) / d(days)
     for (lifter_id, bw_delta_list,) in bw_delta_list_d.items():
         sum_bw_pct = 0
         sum_days = 0
 
         for (delta_bw_pct, delta_days,) in bw_delta_list:
-            sum_bw_pct += float(delta_bw_pct)
-            sum_days += float(delta_days)
 
-        avg_list.append(sum_bw_pct / sum_days)
+            if not bw_pct_per_day2_d.get(delta_days):
+                bw_pct_per_day2_d[delta_days] = []
 
-    return (min(avg_list), max(avg_list),)
+            bw_pct_per_day2_d[delta_days].append(delta_bw_pct / float(delta_days))
+        
+    # now for each day delta find the average bw pct delta
+    for (delta_days, bw_pct_per_day_list,) in bw_pct_per_day2_d.items():
+        avg_bw_pct_per_day2_d[delta_days] = mean(bw_pct_per_day_list)
+
+    # now sort by delta_days
+    sorted_avg_bw_pct_per_day2_items = sorted(avg_bw_pct_per_day2_d.items(), key=lambda item_tup: item_tup[0])
+
+    return sorted_avg_bw_pct_per_day2_items
+
 
 if __name__ == '__main__':
 
-    lifters = CsvReadIter(opl_path('lifters.csv'))
-    meets = CsvReadIter(opl_path('meets.csv'))
-    entries = CsvReadIter(opl_path('entries.csv'))
+    lifters = CsvReadIter(opl_path('lifters.csv'), dict_reader=True)
+    meets = CsvReadIter(opl_path('meets.csv'), dict_reader=True)
+    entries = CsvReadIter(opl_path('entries.csv'), dict_reader=True)
 
     (lifter_name_d, lifter_id_set,) = map_disamb_lifters(lifters)
+    sorted_entry_list = augment_and_sort_entries(entries, meets, lifter_id_set)
+    bw_delta_list_d = collect_bw_data(sorted_entry_list)
+    sorted_avg_bw_pct_per_day2_items = analyse_data(bw_delta_list_d)
 
-    print("loaded {} disambiguated lifters".format(len(lifter_name_d)))
-
-    meet_d = load_meet_data(meets)
-
-    print("loaded {} meets".format(len(meet_d)))
-
-    bw_delta_list_d = collect_bw_data(entries, meet_d, lifter_id_set)
-    (min_avg, max_avg) = analyse_data(bw_delta_list_d)
-
-    print("min average bw%/day: {}".format(min_avg))
-    print("max average bw%/day: {}".format(max_avg))
+    for (delta_days, avg_bw_pct_per_day,) in sorted_avg_bw_pct_per_day2_items:
+        print("Days: {}, Mean bw% difference per day: {}".format(delta_days, avg_bw_pct_per_day))
 
