8bf8ee35de4a00656f238168423fe66c7a1dc6db
Matt Pearce <matthew.pearce10@gmail.com>
2021-08-08 11:31:25 +1000

    Calculate the interval to go from min bw to max bw.

--- checker/src/checklib/consistency/bodyweight.rs
+++ checker/src/checklib/consistency/bodyweight.rs
@@ -4,11 +4,11 @@ use crate::checklib::consistency::{self, date, ConsistencyResult};
 use crate::{AllMeetData, Entry, EntryIndex, LifterDataMap, LifterMap, Report};
 
 // These are extremely loose right now, reasonable values result in having to fix a ludicrous number of errors
-const WEIGHT_CUT_PERCENTAGE_THRESHOLD: f32 = 150.0;
-const TISSUE_CHANGE_PERCENTAGE_THRESHOLD: f32 = 50.0;
+const WEIGHT_CUT_CHANGE_THRESHOLD: f32 = 1.5;
+const TISSUE_CHANGE_THRESHOLD: f32 = 0.5;
 
-/// Get the change in bodyweight from `a` to `b` as a percentage
-fn calc_percentage_bw_change(entry_from: &Entry, entry_to: &Entry) -> f32 {
+/// Get the relative change in bodyweight from `a` to `b`
+fn calc_relative_bw_change(entry_from: &Entry, entry_to: &Entry) -> f32 {
     // Handle division-by-zero cases early.
     if entry_from.bodyweightkg.is_zero() || entry_to.bodyweightkg.is_zero() {
         return 0.0;
@@ -19,8 +19,8 @@ fn calc_percentage_bw_change(entry_from: &Entry, entry_to: &Entry) -> f32 {
     let to_bw = f32::from(entry_to.bodyweightkg);
     let bw_delta = f32::abs(from_bw - to_bw);
 
-    // Express that delta as a percentage change with respect to Entry `a`.
-    (bw_delta / from_bw) * 100.0
+    // Express that delta as a relative change with respect to Entry `a`.
+    bw_delta / from_bw
 }
 
 /// Checks bodyweight consistency for a single lifter.
@@ -58,14 +58,19 @@ pub fn check_bodyweight_one(
 
         let interval_days = ((this_date - prev_date) as f32).abs();
 
-        // Get the percentage change in bodyweight between `prev` and `entry`,
+        // Get the relative change in bodyweight between `prev` and `entry`,
         // we can check if this change makes sense given that a lifter could potentially do back to back 24hour weigh ins
-        //  and with a rate of long term bodyweight change
-        let percentage_change = calc_percentage_bw_change(prev, entry);
+        // and with a rate of long term bodyweight change
+        let relative_change = calc_relative_bw_change(prev, entry);
 
-        if percentage_change.abs()
-            > (WEIGHT_CUT_PERCENTAGE_THRESHOLD
-                + interval_days * TISSUE_CHANGE_PERCENTAGE_THRESHOLD)
+        // Number of days that it would take to go from the minimum allowed bodyweight (15kg) to the maximum (300kg)
+        let max_interval =
+            (300.0_f32.log10() - 15.0_f32.log10() - (1.0 + WEIGHT_CUT_CHANGE_THRESHOLD).log10())
+                / ((1.0 + TISSUE_CHANGE_THRESHOLD).log10());
+
+        if relative_change.abs()
+            > (WEIGHT_CUT_CHANGE_THRESHOLD
+                + max_interval.min(interval_days) * TISSUE_CHANGE_THRESHOLD)
         {
             let days = this_date - prev_date;
             let plural = if days > 1 { "s" } else { "" };
